0f2deebdfb061de42312a48499c7083fed3a68d2
AlterOtter <oksonsango2@gmail.com>
2022-01-28 16:54:19 +0900

    0128-1654

--- mysite02/src/main/java/com/poscoict/mysite/config/ConfigMysite.java
+++ mysite02/src/main/java/com/poscoict/mysite/config/ConfigMysite.java
@@ -2,7 +2,7 @@ package com.poscoict.mysite.config;
 
 public class ConfigMysite {
 	public static final String IMG_PATH="D:/poscoict/servletworkspace/mysite/mysite02/src/main/webapp/assets/images"; 
-	public static final String URL = "jdbc:mysql://127.0.0.1:3307/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
+	public static final String URL = "jdbc:mysql://192.168.0.73:3307/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
 	public static final String USER = "webdb";
 	public static final String PASSWORD = "webdb";
 }
