d8e4b87793f64c4ea255e61c7e7ae69dca58efac
AlterOtter <oksonsango2@gmail.com>
2022-03-02 11:50:57 +0900

    Jackson 적용 완료

--- mysite05/src/main/java/com/poscoict/config/web/MvcConfig.java
+++ mysite05/src/main/java/com/poscoict/config/web/MvcConfig.java
@@ -1,9 +1,17 @@
 package com.poscoict.config.web;
 
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -38,6 +46,32 @@ public class MvcConfig extends WebMvcConfigurerAdapter{
 	}
 	
 	//MessageConvertor
+		@Bean
+		public StringHttpMessageConverter stringHttpMessageConverter() {
+			StringHttpMessageConverter messageConverter = new StringHttpMessageConverter();
+			messageConverter.setSupportedMediaTypes(Arrays.asList(
+					new MediaType("text","html",Charset.forName("utf-8"))
+					
+					));
+			return messageConverter;
+		}
+
+		@Bean 
+		public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+			MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter(new Jackson2ObjectMapperBuilder()
+																.indentOutput(true).dateFormat(new SimpleDateFormat("yyyy-MM-dd")).build());
+			messageConverter.setSupportedMediaTypes(Arrays.asList(
+					new MediaType("application","json",Charset.forName("utf-8"))
+					
+					));
+			return messageConverter;
+		}
+		
+		@Override
+		public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+			converters.add(stringHttpMessageConverter());
+			converters.add(mappingJackson2HttpMessageConverter());
+		}
 
 	
 	
